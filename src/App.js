6562f4fab0ea301382d66378bcd822c65880827e
Hassan12-ui <ijazhassan9150@gmail.com>
2024-07-03 17:09:26 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Testing New App</h1>
+     <button>Register</button>
+    </div>
+  );
+}
+
+export default App;
